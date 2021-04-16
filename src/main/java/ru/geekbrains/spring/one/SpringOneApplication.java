ac6f64fdc7946ebfcca7f9808de06685c4954e22
KatySid <whitekaty666@gmail.com>
2021-04-16 18:35:24 +0400

    Initial commit

---
+++ src/main/java/ru/geekbrains/spring/one/SpringOneApplication.java
@@ -0,0 +1,18 @@
+package ru.geekbrains.spring.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOneApplication {
+	// Домашнее задание
+	// 1. На главной странице сделать форму для поиска студента по Id
+	// вбили id - перешли на страницу показа соответствующего студента (страница есть)
+	// 2. Добавьте на странице со списком студентов по бокам балла две кнопки "-" и "+",
+	// при нажатии на которые должен происходить декремент и инкремент балла студента.
+	// Балл не может быть меньше 0 и больше 100
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOneApplication.class, args);
+	}
+}
